3083047e7b457f2813fcdc9158a71d59a1ba09cc
ViniciusGomesBrandao <macarraojovial@gmail.com>
2022-03-15 18:27:43 -0300

    Simplificacao

--- __init__.py
+++ __init__.py
@@ -1,4 +1,10 @@
 import trello
 client = trello.client('14b1070ecb225a3e0eb448f40c0f2ad9', 'a6f0ecacfc74673735a9091bb212f67edc5d66c06bc6f8b760b40ef12dd2d0d2')
-data = client.get("/boards/622f5e933b6a1e11b6885461")
+
+
+
+board = "622f5e933b6a1e11b6885461"
+
+
+data = client.get("/boards/"+board)
 print({data['name']})
